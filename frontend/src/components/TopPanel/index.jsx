19ea2aa4d16dab4b93c856b1c63fbf4bfade7164
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-18 04:03:13 +0300

    Add top panel

---
+++ frontend/src/components/TopPanel/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TopPanel() {
+  return <div></div>;
+}
+
+export default TopPanel;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import styles from "./Home.module.scss";
+import TopPanel from "../../components/TopPanel";
 
 function Home() {
-  return <></>;
+  return (
+    <>
+      <TopPanel />
+    </>
+  );
 }
 
 export default Home;
