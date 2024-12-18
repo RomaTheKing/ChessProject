bca16aaf7279398765ccec86a561fd03831e0cf0
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-18 14:34:13 +0300

    Add top panel

--- frontend/src/components/TopPanel/index.jsx
+++ frontend/src/components/TopPanel/index.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
+import styles from "./TopPanel.module.scss";
+
+const elements = [
+  {
+    name: "Игра",
+  },
+  {
+    name: "Задачи",
+  },
+  {
+    name: "Обучение",
+  },
+  {
+    name: "Просмотр",
+  },
+  {
+    name: "Сообщество",
+  },
+  {
+    name: "Инструменты",
+  },
+];
+
 function TopPanel() {
-  return <div></div>;
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <text className={styles.title}>Super Chess</text>
+        {elements.map((v, i) => (
+          <div className={styles.item}>
+            <text>{v.name}</text>
+          </div>
+        ))}
+        <input className={styles.search}></input>
+      </div>
+    </div>
+  );
 }
 
 export default TopPanel;
