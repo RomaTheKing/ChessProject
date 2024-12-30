ced859c5827a1f3b19c5c67885eaa74ad6dc959f
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-30 19:32:43 +0300

    Frotend: add game page to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import AuthPage from "./pages/Auth";
 import HomePage from "./pages/Home";
+import GamePage from "./pages/Game";
 import { Route, Routes } from "react-router-dom";
 import RegPage from "./pages/Reg";
 import "./scss/app.scss";
@@ -9,6 +10,7 @@ function App() {
     <div className="root">
       <Routes>
         <Route path="/" element={<AuthPage />} />
+        <Route path="/Game" element={<GamePage />} />
         <Route path="/Reg" element={<RegPage />} />
         <Route path="/Home" element={<HomePage />} />
       </Routes>
