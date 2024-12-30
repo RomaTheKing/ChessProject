2d8179d9c6aaaa3db8755ab9986e0aa74ba526b0
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-30 19:29:54 +0300

    Frontend: add game page

---
+++ frontend/src/pages/Game/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GameBoard from "../../components/game/GameBoard";
+
+function Game() {
+  return (
+    <div>
+      <div>
+        <GameBoard />
+      </div>
+    </div>
+  );
+}
+
+export default Game;
