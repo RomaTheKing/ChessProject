073b6a9cefd6f22e647f9217bc89a16f13026630
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-30 19:31:01 +0300

    Frontend: add Unit component

---
+++ frontend/src/components/game/Unit/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Unit.module.scss";
+
+function Unit() {
+  return <img className={styles.unit} src="/units/white/king.png"></img>;
+}
+
+export default Unit;
