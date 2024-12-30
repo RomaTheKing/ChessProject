94f90f34014e63ab517c7d05e1ee7b0dd42f1130
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-30 19:29:21 +0300

    Frontend: fix Reg page

--- frontend/src/pages/Reg/index.jsx
+++ frontend/src/pages/Reg/index.jsx
@@ -7,19 +7,32 @@ function Reg() {
   return (
     <div className={styles.root}>
       <text className={styles.title}>Регистрация</text> <br />
-      <text className={styles.name_title}>Имя</text>
+      <div className={styles.input_div}>
+        <text className={styles.input_title}>Имя</text>
+        <br />
+        <input placeholder="Имя пользователя" type="login"></input>
+      </div>
       <br />
-      <input placeholder="Имя пользователя" type="login"></input> <br />
-      <text className={styles.login_title}>Логин</text>
+      <div className={styles.input_div}>
+        <text className={styles.input_title}>Логин</text>
+        <br />
+        <input placeholder="Логин" type="login"></input>
+      </div>
       <br />
-      <input placeholder="Логин" type="login"></input> <br />
-      <text className={styles.password_title}>Пароль</text>
+      <div className={styles.input_div}>
+        <text className={styles.input_title}>Пароль</text>
+        <br />
+        <input placeholder="Пароль" type="password"></input>
+      </div>
       <br />
-      <input placeholder="Пароль" type="password"></input> <br />
-      <text className={styles.password_repeat_title}>Повторите Пароль</text>
-      <br />
-      <input placeholder="Повторите Пароль" type="password"></input> <br />
-      <button>Регистрация</button> <br />
+      <div className={styles.input_div}>
+        <text className={styles.input_title}>Повторите Пароль</text>
+        <br />
+        <input placeholder="Повторите Пароль" type="password"></input>
+      </div>
+      <div className={styles.button_div}>
+        <button>Регистрация</button>
+      </div>
     </div>
   );
 }
