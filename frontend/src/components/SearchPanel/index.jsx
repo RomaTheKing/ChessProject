1524aeba494dcb899de5957f6d87c2533546cd4c
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-30 19:30:32 +0300

    Frontend: Rename Monitor to Search panel

---
+++ frontend/src/components/SearchPanel/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "./SearchPanel.module.scss";
+import GameLabel from "../GameLabel";
+
+function SearchPanel() {
+  return (
+    <div className={styles.root}>
+      <GameLabel
+        name={"valavshonok"}
+        rating={1234}
+        timeControl={{ base: 5, delta: 3 }}
+        mode={0}
+      />
+      <GameLabel
+        name={"The_Andrey"}
+        rating={1233}
+        timeControl={{ base: 8, delta: 0 }}
+        mode={1}
+      />
+    </div>
+  );
+}
+
+export default SearchPanel;
