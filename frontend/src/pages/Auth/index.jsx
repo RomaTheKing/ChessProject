163e36e276029af0c7ffb0a9bd0aac796967923a
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-12-30 19:28:55 +0300

    Frontend: fix auth page

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -7,15 +7,22 @@ function Auth() {
   return (
     <div className={styles.root}>
       <text className={styles.title}>Авторизация</text> <br />
-      <text className={styles.login_title}>Логин</text>
+      <div className={styles.input_div}>
+        <text className={styles.input_title}>Логин</text>
+        <br />
+        <input placeholder="Логин" type="login"></input> <br />
+      </div>
       <br />
-      <input placeholder="Логин" type="login"></input> <br />
-      <text className={styles.password_title}>Пароль</text>
-      <br />
-      <input placeholder="Пароль" type="password"></input> <br />
-      <Link to="/home">
-        <button>Войти</button> <br />
-      </Link>
+      <div className={styles.input_div}>
+        <text className={styles.input_title}>Пароль</text>
+        <br />
+        <input placeholder="Пароль" type="password"></input> <br />
+      </div>
+      <div className={styles.button_div}>
+        <Link to="/home">
+          <button>Войти</button> <br />
+        </Link>
+      </div>
       <Link to="Reg" className={styles.reg_link}>
         Регистрация
       </Link>
