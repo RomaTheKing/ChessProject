3dfd5cd093ca7d0401a133d96c0eb69e67c5cd4a
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2025-01-03 00:46:33 +0300

    Frontend: Add MainTitle to Game page

--- frontend/src/pages/Game/index.jsx
+++ frontend/src/pages/Game/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import GameBoard from "../../components/game/GameBoard";
 
+import styles from "./Game.module.scss";
+
 function Game() {
   return (
     <div>
+      <div className={styles.MainTitle_div}>
+        <text>Super Chess</text>
+      </div>
       <div>
         <GameBoard />
       </div>
