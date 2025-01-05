ee22cdc7a6be90dfe9514d5e0362a4366d117b37
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2025-01-05 05:04:16 +0300

    Frontend: Add Error displaying support to Auth page

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import styles from "./Auth.module.scss";
 import { Link } from "react-router-dom";
 
 function Auth() {
+  const email = useRef();
+  const password = useRef();
+
+  const [emailErrorID, setEmailErrorID] = useState(0);
+  const [passwordErrorID, setPasswordErrorID] = useState(0);
+
   return (
     <div>
       <div className={styles.MainTitle_div}>
@@ -14,20 +20,37 @@ function Auth() {
         <div className={styles.input_div}>
           <text className={styles.input_title}>Логин</text>
           <br />
-          <input placeholder="Логин" type="login"></input> <br />
+          <input ref={email} placeholder="Логин" type="login"></input> <br />
+          {emailErrorID === 1 && (
+            <text className={styles.error}>Логин не должен быть пустым</text>
+          )}
         </div>
         <br />
         <div className={styles.input_div}>
           <text className={styles.input_title}>Пароль</text>
           <br />
-          <input placeholder="Пароль" type="password"></input> <br />
+          <input ref={password} placeholder="Пароль" type="password"></input>
+          <br />
+          {passwordErrorID === 1 && (
+            <text className={styles.error}>Пароль не должен быть пустым</text>
+          )}
         </div>
         <div className={styles.button_div}>
           <Link to="/home">
-            <button>Войти</button> <br />
+            <button
+              onClick={() => {
+                setEmailErrorID(0);
+                setPasswordErrorID(0);
+                if (email.current.value === "") setEmailErrorID(1);
+                if (password.current.value === "") setPasswordErrorID(1);
+              }}
+            >
+              Войти
+            </button>{" "}
+            <br />
           </Link>
         </div>
-        <Link to="Reg" className={styles.reg_link}>
+        <Link to="/Reg" className={styles.reg_link}>
           Регистрация
         </Link>
       </div>
