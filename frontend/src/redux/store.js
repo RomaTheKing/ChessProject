76c4dd089befbf6bac494e1d3c907790e3afb354
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2025-01-07 14:47:11 +0300

    Frontend: Add auth store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import board from "./slices/boardSlice";
 import searchSlice from "./slices/searchSlice";
 import messageSlice from "./slices/messageSlice";
+import authSlice from "./slices/authSlice";
 
 export default configureStore({
   reducer: {
     board,
     searchSlice,
     messageSlice,
+    authSlice,
   },
 });
