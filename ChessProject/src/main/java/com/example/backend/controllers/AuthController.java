8609863cc31d9890b3fa013f46b78b7bfa6e5da3
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2025-01-07 14:46:27 +0300

    Backend: Add auth support

--- ChessProject/src/main/java/com/example/backend/controllers/AuthController.java
+++ ChessProject/src/main/java/com/example/backend/controllers/AuthController.java
@@ -1,4 +1,43 @@
 package com.example.backend.controllers;
 
+import com.example.backend.auth.JwtUtils;
+import com.example.backend.requests.AuthRequest;
+import com.example.backend.responses.JWTResponse;
+import com.example.backend.services.user.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/api/auth")
 public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authUser(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+//        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//        List<String> roles = userDetails.getAuthorities().stream()
+//                .map(item -> item.getAuthority())
+//                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JWTResponse(jwt));
+    }
 }
